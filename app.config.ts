19362ef7c10b017dfb0c5259784d5e312c6ed5d0
Danny Brisson <brisson.danny@gmail.com>
2023-08-30 18:57:01 -0400

    first test

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  // Configure Content Wind here
+  cover: '/cover.jpg',
+  socials: {
+    twitter: '?',
+    github: '?'
+  },
+  prose: {
+    h1: {
+      icon: 'carbon:sailboat-coastal'
+    }
+  }
+})
